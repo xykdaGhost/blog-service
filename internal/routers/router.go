8c9ea2b0065888a8f5c642c9c56a03fa187741ba
xykdaGhost <yikaixiong@gmail.com>
2022-11-12 18:39:27 +0800

    2.4

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,13 +2,17 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-programming-tour-book/blog-service/docs"
 	v1 "github.com/go-programming-tour-book/blog-service/internal/routers/api/v1"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	article := v1.NewArticle()
 	tag := v1.NewTag()
 
