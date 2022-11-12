57a1ea47cd6eca28173027bcac98c811efa683d9
xykdaGhost <yikaixiong@gmail.com>
2022-11-12 18:11:39 +0800

    2.3.3

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,14 @@
 package model
 
+import (
+	"fmt"
+
+	"github.com/go-programming-tour-book/blog-service/global"
+	"github.com/go-programming-tour-book/blog-service/pkg/setting"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedBy  string `json:"created_by"`
@@ -8,4 +17,28 @@ type Model struct {
 	ModifiedOn uint32 `json:"modified_on"`
 	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_del"`
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local"
+	db, err := gorm.Open(databaseSetting.DBType, fmt.Sprintf(s,
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime,
+	))
+	if err != nil {
+		return nil, err
+	}
+
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
+
+	return db, nil
 }
